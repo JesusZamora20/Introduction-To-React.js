8d50a1a9620211828735d96466fcccb11256b43b
Jesus Zamora <jbermudezamora87@gmail.com>
2022-10-21 22:03:33 -0500

    State handling in react

--- react-initial-course/src/ToDoSearch.js
+++ react-initial-course/src/ToDoSearch.js
@@ -2,17 +2,23 @@ import React from "react";
 import './ToDoSearch.css';
 
 function ToDoSearch(){
+    const [searchValue, setSearchValue] = React.useState('');
+
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
-    return(
+    return[
         <input 
         className="ToDoSearch" 
         placeholder='Write your ToDo'
+        value = {searchValue}
         onChange={onSearchValueChange}
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 
 export {ToDoSearch};
